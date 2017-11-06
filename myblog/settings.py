8869502694e36805cb51e66c5fbb8ad5847a343c
home-17 <xiongzhibiao@calfdata.com>
2017-11-06 23:32:11 +0800

    配置副文本和后台

--- myblog/settings.py
+++ myblog/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
@@ -204,3 +216,6 @@ LOGGING = {
     }
 }
 
+
+# 自定义用户model
+AUTH_USER_MODEL = 'blog.User'
